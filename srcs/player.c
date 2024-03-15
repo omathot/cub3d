ba34840c9f2f73237ae15417006b6773c6c89e64
omathot <116487276+omathot@users.noreply.github.com>
2024-03-15 12:07:30 +0100

    further norminette work, mainly on player.c, also added changes in .h for necesssary structs

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: oscarmathot <oscarmathot@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 13:14:21 by oscarmathot       #+#    #+#             */
-/*   Updated: 2024/03/13 17:42:38 by oscarmathot      ###   ########.fr       */
+/*   Updated: 2024/03/15 12:06:30 by oscarmathot      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,12 +121,102 @@ int	cast_ray(char **board, t_point *player, int angle)
 //   return (0);
 // }
 
+void	move_forward(t_param_mlx *param, float angle_radians, float move_speed)
+{
+	if (!cast_ray(param->map.board, &param->map.player.pos,
+			param->map.player.angle))
+	{
+		param->map.player.pos.x += sin(angle_radians) * move_speed;
+		param->map.player.pos.y -= cos(angle_radians) * move_speed;
+		update_current_wall(&param->current_visible_walls,
+			param->map, param->x_resolution);
+	}
+}
+
+void	move_backwards(t_param_mlx *param,
+	float angle_radians, float move_speed)
+{
+	int	coll_angle;
+
+	coll_angle = (int)(param->map.player.angle + 180) % 360;
+	if (!cast_ray(param->map.board,
+			&param->map.player.pos, coll_angle))
+	{
+		param->map.player.pos.x -= sin(angle_radians) * move_speed;
+		param->map.player.pos.y += cos(angle_radians) * move_speed;
+		update_current_wall(&param->current_visible_walls,
+			param->map, param->x_resolution);
+	}
+}
+
+void	move_left(t_param_mlx *param, float angle_radians, float move_speed)
+{
+	int	coll_angle;
+
+	coll_angle = (int)(param->map.player.angle + 270) % 360;
+	if (!cast_ray(param->map.board,
+			&param->map.player.pos, coll_angle))
+	{
+		param->map.player.pos.x -= cos(angle_radians) * move_speed;
+		param->map.player.pos.y -= sin(angle_radians) * move_speed;
+		update_current_wall(&param->current_visible_walls,
+			param->map, param->x_resolution);
+	}
+}
+
+void	move_right(t_param_mlx *param, float angle_radians, float move_speed)
+{
+	int	coll_angle;
+
+	coll_angle = (int)(param->map.player.angle + 90) % 360;
+	if (!cast_ray(param->map.board,
+			&param->map.player.pos, coll_angle))
+	{
+		param->map.player.pos.x += cos(angle_radians) * move_speed;
+		param->map.player.pos.y += sin(angle_radians) * move_speed;
+		update_current_wall(&param->current_visible_walls,
+			param->map, param->x_resolution);
+	}
+}
+
+void	handle_movement(t_param_mlx *param,
+	float angle_radians, float move_speed, int mode)
+{
+	if (mode == 1)
+		move_forward(param, angle_radians, move_speed);
+	if (mode == 2)
+		move_backwards(param, angle_radians, move_speed);
+	if (mode == 3)
+		move_left(param, angle_radians, move_speed);
+	if (mode == 4)
+		move_right(param, angle_radians, move_speed);
+}
+
+void	handle_angle(t_param_mlx *param, int mode)
+{
+	if (mode == 1)
+	{
+		if (param->map.player.angle <= 0)
+			param->map.player.angle = 360;
+		param->map.player.angle -= 2;
+		update_current_wall(&param->current_visible_walls, param->map,
+			param->x_resolution);
+	}
+	if (mode == 2)
+	{
+		if (param->map.player.angle >= 360)
+			param->map.player.angle = 0;
+		param->map.player.angle += 2;
+		update_current_wall(&param->current_visible_walls, param->map,
+			param->x_resolution);
+	}
+}
+
 void	player_move(void *param)
 {
 	t_param_mlx	*param_real;
 	float		move_speed;
 	float		angle_radians;
-	int			coll_angle;
 
 	param_real = (t_param_mlx *)param;
 	move_speed = 0.03;
@@ -135,66 +225,15 @@ void	player_move(void *param)
 		mlx_close_window(param_real->mlx);
 	angle_radians = param_real->map.player.angle * M_PI / 180;
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_W))
-	{
-		if (!cast_ray(param_real->map.board, &param_real->map.player.pos,
-				param_real->map.player.angle))
-		{
-			param_real->map.player.pos.x += sin(angle_radians) * move_speed;
-			param_real->map.player.pos.y -= cos(angle_radians) * move_speed;
-			update_current_wall(&param_real->current_visible_walls,
-				param_real->map, param_real->x_resolution);
-		}
-	}
+		handle_movement(param, angle_radians, move_speed, 1);
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_S))
-	{
-		coll_angle = (int)(param_real->map.player.angle + 180) % 360;
-		if (!cast_ray(param_real->map.board,
-				&param_real->map.player.pos, coll_angle))
-		{
-			param_real->map.player.pos.x -= sin(angle_radians) * move_speed;
-			param_real->map.player.pos.y += cos(angle_radians) * move_speed;
-			update_current_wall(&param_real->current_visible_walls,
-				param_real->map, param_real->x_resolution);
-		}
-	}
+		handle_movement(param, angle_radians, move_speed, 2);
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_A))
-	{
-		coll_angle = (int)(param_real->map.player.angle + 270) % 360;
-		if (!cast_ray(param_real->map.board,
-				&param_real->map.player.pos, coll_angle))
-		{
-			param_real->map.player.pos.x -= cos(angle_radians) * move_speed;
-			param_real->map.player.pos.y -= sin(angle_radians) * move_speed;
-			update_current_wall(&param_real->current_visible_walls,
-				param_real->map, param_real->x_resolution);
-		}
-	}
+		handle_movement(param, angle_radians, move_speed, 3);
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_D))
-	{
-		coll_angle = (int)(param_real->map.player.angle + 90) % 360;
-		if (!cast_ray(param_real->map.board,
-				&param_real->map.player.pos, coll_angle))
-		{
-			param_real->map.player.pos.x += cos(angle_radians) * move_speed;
-			param_real->map.player.pos.y += sin(angle_radians) * move_speed;
-			update_current_wall(&param_real->current_visible_walls,
-				param_real->map, param_real->x_resolution);
-		}
-	}
+		handle_movement(param, angle_radians, move_speed, 4);
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_Q))
-	{
-		if (param_real->map.player.angle <= 0)
-			param_real->map.player.angle = 360;
-		param_real->map.player.angle -= 2;
-		update_current_wall(&param_real->current_visible_walls, param_real->map,
-			param_real->x_resolution);
-	}
+		handle_angle(param, 1);
 	if (mlx_is_key_down(param_real->mlx, MLX_KEY_E))
-	{
-		if (param_real->map.player.angle >= 360)
-			param_real->map.player.angle = 0;
-		param_real->map.player.angle += 2;
-		update_current_wall(&param_real->current_visible_walls, param_real->map,
-			param_real->x_resolution);
-	}
+		handle_angle(param, 2);
 }
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,15 +6,15 @@
 /*   By: oscarmathot <oscarmathot@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 14:07:53 by oscarmathot       #+#    #+#             */
-/*   Updated: 2024/02/06 15:27:59 by oscarmathot      ###   ########.fr       */
+/*   Updated: 2024/03/15 11:40:31 by oscarmathot      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-t_point mk_point(double x, double y)
+t_point	mk_point(double x, double y)
 {
-	t_point new_point;
+	t_point	new_point;
 
 	new_point.x = x;
 	new_point.y = y;
@@ -23,9 +23,9 @@ t_point mk_point(double x, double y)
 
 // void	print_point(char *start_message, t_point point);
 
-t_point *mk_point_malloc(double x, double y)
+t_point	*mk_point_malloc(double x, double y)
 {
-	t_point *new_point;
+	t_point	*new_point;
 
 	new_point = malloc(sizeof(t_point *));
 	new_point->x = x;
@@ -33,4 +33,3 @@ t_point *mk_point_malloc(double x, double y)
 	print_point("new point made with value: ", (*new_point));
 	return (new_point);
 }
-
